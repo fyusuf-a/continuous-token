f81cc1d6a553b89e9934469275e844e92fad9db4
BatteryHorseStaple <67638160+EPFPhmiw47mosLJR@users.noreply.github.com>
2026-02-23 16:46:00 +0530

    fix: Add vaults to init tests

--- tests/01_initialize.ts
+++ tests/01_initialize.ts
@@ -1,42 +1,16 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { ContinuousToken } from "../target/types/continuous_token";
-import { createMint, getAssociatedTokenAddressSync, getOrCreateAssociatedTokenAccount, mintTo, TOKEN_2022_PROGRAM_ID, getTokenMetadata } from "@solana/spl-token";
 import assert from "node:assert/strict";
+import type { Program } from "@coral-xyz/anchor";
+import * as anchor from "@coral-xyz/anchor";
+import {
+  createMint,
+  getAssociatedTokenAddressSync,
+  getTokenMetadata,
+  TOKEN_2022_PROGRAM_ID,
+} from "@solana/spl-token";
+import { BN } from "bn.js";
 import { expect } from "chai";
-
-const RT_DECIMALS = 6;
-
-const airdrop = async (provider: anchor.AnchorProvider, publicKey: anchor.web3.PublicKey) => {
-  const signature = await provider.connection.requestAirdrop(publicKey, 10 * anchor.web3.LAMPORTS_PER_SOL);
-  const latestBlockHash = await provider.connection.getLatestBlockhash();
-  await provider.connection.confirmTransaction({
-    signature,
-    ...latestBlockHash,
-  }, "confirmed");
-}
-
-const airdropRT = async (provider: anchor.AnchorProvider, mintRT: anchor.web3.PublicKey, destination: anchor.web3.PublicKey) => {
-  const ata = await getOrCreateAssociatedTokenAccount(
-    provider.connection,
-    provider.wallet.payer,
-    mintRT,
-    destination,
-  );
-
-  await mintTo(
-    provider.connection,
-    provider.wallet.payer,
-    mintRT,
-    ata.address,
-    provider.wallet.publicKey,
-    1000000,
-  );
-}
-
-const randomBN = (max: number) => {
-  return new anchor.BN(Math.floor(Math.random() * max));
-}
+import type { ContinuousToken } from "../target/types/continuous_token";
+import { airdrop, airdropRT, RT_DECIMALS, randomBN } from "./helpers";
 
 describe("Program initialization", () => {
   const provider = anchor.AnchorProvider.env();
@@ -47,8 +21,10 @@ describe("Program initialization", () => {
   const initializer = provider.wallet.publicKey;
   let mintRt: anchor.web3.PublicKey;
   let vaultRt: anchor.web3.PublicKey;
+  let vaultCtUnlocked: anchor.web3.PublicKey;
+  let vaultCtLocked: anchor.web3.PublicKey;
   const seed = randomBN(1000);
-  const firstPrice = new anchor.BN(0.01 * 10 ** RT_DECIMALS); // 0.01 RT
+  const firstPrice = new BN(0.01 * 10 ** RT_DECIMALS); // 0.01 RT
   const reserveRatioBps = 9_000; // 90%
   const baseFeeBps = 100; // 1%
   const discountBps = 50; // .5%
@@ -62,6 +38,10 @@ describe("Program initialization", () => {
   );
   const [ctMintPda] = anchor.web3.PublicKey.findProgramAddressSync(
     [Buffer.from("ct"), seed.toArrayLike(Buffer, "le", 8)],
+    program.programId,
+  );
+  const [feeVaultLockedPda] = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from("fee_vault"), seed.toArrayLike(Buffer, "le", 8)],
     program.programId,
   );
 
@@ -79,68 +59,96 @@ describe("Program initialization", () => {
     await airdropRT(provider, mintRt, initializer);
 
     vaultRt = getAssociatedTokenAddressSync(mintRt, configPda, true);
+
+    vaultCtUnlocked = getAssociatedTokenAddressSync(
+      ctMintPda,
+      configPda,
+      true,
+      TOKEN_2022_PROGRAM_ID,
+      anchor.utils.token.ASSOCIATED_PROGRAM_ID,
+    );
+    vaultCtLocked = getAssociatedTokenAddressSync(
+      ctMintPda,
+      feeVaultLockedPda,
+      true,
+      TOKEN_2022_PROGRAM_ID,
+      anchor.utils.token.ASSOCIATED_PROGRAM_ID,
+    );
   });
 
   it("Program fails to initialize if discount > fee", async () => {
-    let tests = [
+    const tests = [
       { baseFeeBps: 100, discountBps: 150 },
       { baseFeeBps: 100, discountBps: 100 },
     ];
 
-    for (let test of tests) {
-      await assert.rejects(async () => {
-        await program.methods.initialize(
-          seed,
-          firstPrice,
-          reserveRatioBps,
-          test.baseFeeBps,
-          test.discountBps,
-          rtTokenName,
-          rtTokenSymbol,
-          rtTokenUri,
-        ).accountsStrict({
-          initializer,
-          config: configPda,
-          mintCt: ctMintPda,
-          mintRt,
-          vaultRt,
-          tokenProgramRt: anchor.utils.token.TOKEN_PROGRAM_ID,
-          tokenProgramCt: anchor.utils.token.TOKEN_PROGRAM_ID,
-          associatedTokenProgram: anchor.utils.token.ASSOCIATED_PROGRAM_ID,
-          systemProgram: anchor.web3.SystemProgram.programId,
-        }).rpc();
-      },
+    for (const test of tests) {
+      await assert.rejects(
+        async () => {
+          await program.methods
+            .initialize(
+              seed,
+              firstPrice,
+              reserveRatioBps,
+              test.baseFeeBps,
+              test.discountBps,
+              rtTokenName,
+              rtTokenSymbol,
+              rtTokenUri,
+            )
+            .accountsStrict({
+              initializer,
+              config: configPda,
+              mintRt,
+              mintCt: ctMintPda,
+              vaultRt,
+              vaultCtLocked,
+              vaultCtUnlocked,
+              feeVaultAuthority: feeVaultLockedPda,
+              tokenProgramRt: anchor.utils.token.TOKEN_PROGRAM_ID,
+              tokenProgramCt: anchor.utils.token.TOKEN_PROGRAM_ID,
+              associatedTokenProgram: anchor.utils.token.ASSOCIATED_PROGRAM_ID,
+              systemProgram: anchor.web3.SystemProgram.programId,
+            })
+            .rpc();
+        },
         () => true,
-        "Config should fail"
+        "Config should fail",
       );
-    };
+    }
   });
 
   it("Program successfully initializes", async () => {
-    await program.methods.initialize(
-      seed,
-      firstPrice,
-      reserveRatioBps,
-      baseFeeBps,
-      discountBps,
-      rtTokenName,
-      rtTokenSymbol,
-      rtTokenUri,
-    ).accountsStrict({
-      initializer,
-      config: configPda,
-      mintCt: ctMintPda,
-      mintRt,
-      vaultRt,
-      tokenProgramRt: anchor.utils.token.TOKEN_PROGRAM_ID,
-      tokenProgramCt: TOKEN_2022_PROGRAM_ID,
-      associatedTokenProgram: anchor.utils.token.ASSOCIATED_PROGRAM_ID,
-      systemProgram: anchor.web3.SystemProgram.programId,
-    }).rpc();
+    await program.methods
+      .initialize(
+        seed,
+        firstPrice,
+        reserveRatioBps,
+        baseFeeBps,
+        discountBps,
+        rtTokenName,
+        rtTokenSymbol,
+        rtTokenUri,
+      )
+      .accountsStrict({
+        initializer,
+        config: configPda,
+        mintRt,
+        mintCt: ctMintPda,
+        vaultRt,
+        vaultCtLocked,
+        vaultCtUnlocked,
+        feeVaultAuthority: feeVaultLockedPda,
+        tokenProgramRt: anchor.utils.token.TOKEN_PROGRAM_ID,
+        tokenProgramCt: TOKEN_2022_PROGRAM_ID,
+        associatedTokenProgram: anchor.utils.token.ASSOCIATED_PROGRAM_ID,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .rpc();
   });
 
   it("After intialization, config account is correctly initialized", async () => {
-    let config = await program.account.config.fetch(configPda);
+    const config = await program.account.config.fetch(configPda);
     expect(config.seed.toString()).to.equal(seed.toString());
     expect(config.firstPrice.toString()).to.equal(firstPrice.toString());
     expect(config.discountBps.toString()).to.equal(discountBps.toString());
@@ -152,21 +160,25 @@ describe("Program initialization", () => {
   it("After initialization, vault RT account is correctly initialized", async () => {
     const vaultAccount = await provider.connection.getAccountInfo(vaultRt);
     expect(vaultAccount).to.not.be.null;
-    expect(vaultAccount?.owner.toString()).to.equal(anchor.utils.token.TOKEN_PROGRAM_ID.toString());
-    const vaultBalance = await provider.connection.getTokenAccountBalance(vaultRt);
+    expect(vaultAccount?.owner.toString()).to.equal(
+      anchor.utils.token.TOKEN_PROGRAM_ID.toString(),
+    );
+    const vaultBalance = await provider.connection.getTokenAccountBalance(
+      vaultRt,
+    );
     expect(vaultBalance.value.uiAmount).to.equal(0);
   });
 
   it("After initialization, continuous token has the correct metadata", async () => {
     const mintAccount = await provider.connection.getAccountInfo(ctMintPda);
     expect(mintAccount).to.not.be.null;
-    expect(mintAccount?.owner.toString()).to.equal(TOKEN_2022_PROGRAM_ID.toString());
+    expect(mintAccount?.owner.toString()).to.equal(
+      TOKEN_2022_PROGRAM_ID.toString(),
+    );
     const metadata = await getTokenMetadata(provider.connection, ctMintPda);
     expect(metadata.updateAuthority.toString()).to.equal(configPda.toString());
     expect(metadata.name).to.equal(rtTokenName);
     expect(metadata.symbol).to.equal(rtTokenSymbol);
     expect(metadata.uri).to.equal(rtTokenUri);
   });
-
-
 });
